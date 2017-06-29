c885d66663e7c2e700dc2d13f42dd9daf17d10ea
Michael Milford <mnmilford@gmail.com>
2017-06-29 15:25:28 -0500

    Remove all easyPieChart from index and main.js

--- main.js
+++ main.js
@@ -33,21 +33,7 @@ $(document).ready(function() {
          }
 
     });
-    
-    /* ======= Chart ========= */
-    
-    $('.chart').easyPieChart({    
-    barColor:'#00BCD4',//Pie chart colour
-    trackColor: '#e8e8e8',
-    scaleColor: false,
-    lineWidth : 5,
-    animate: 2000,
-    onStep: function(from, to, percent) {
-      $(this.el).find('span').text(Math.round(percent));
-    }
-  });  
   
-
     
     /* ======= Isotope plugin ======= */
     /* Ref: http://isotope.metafizzy.co/ */
